9c16508eea9fe738c460e26b5ea327352af0b0c6
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-05-12 11:46:14 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,9 @@
 def using_until
   levitation_force = 6
   #your code here
-until counter == 10
+  
+until leviatation_force == 10 do
   puts "Wingardium Leviosa is equal to 10"
-  counter += 1
+  leviation_force += 1
 end
+end 
